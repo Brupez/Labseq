b6a574f830038d83bb8fe39e9813b17911630c00
BrunoLopes <brunolop50@gmail.com>
2025-09-25 15:42:03 +0100

    full stack app working

--- backend/src/main/java/pt/altice/labSeqDTO.java
+++ backend/src/main/java/pt/altice/labSeqDTO.java
@@ -1,14 +1,13 @@
 package pt.altice;
 
-import java.math.BigInteger;
 
 public class labSeqDTO {
     private int index;
-    private BigInteger value;
+    private String value;
 
     public labSeqDTO() {}
 
-    public labSeqDTO(int index, BigInteger value) {
+    public labSeqDTO(int index, String value) {
         this.index=index;
         this.value=value;
     }
@@ -21,11 +20,11 @@ public class labSeqDTO {
         this.index = index;
     }
 
-    public BigInteger getValue() {
+    public String getValue() {
         return value;
     }
 
-    public void setValue(BigInteger value) {
+    public void setValue(String value) {
         this.value = value;
     }
 }
